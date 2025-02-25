9b53adbc3da794657a9018d6042d94569d3b844c
MarceloFormentini <marceloformentini74@gmail.com>
2025-02-25 13:10:13 -0300

    gerenciador de tarefas

---
+++ src/test/java/br/com/marcelo/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelo.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
